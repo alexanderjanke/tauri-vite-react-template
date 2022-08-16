aacf946f7eac4eed24136674b80197b805225b5a
Alexander Janke <alexander.janke.92@gmail.com>
2022-08-16 11:18:41 +0200

    Setup files

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+  },
+  plugins: ["@typescript-eslint", "simple-import-sort", "prettier"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  rules: {
+    eqeqeq: "error",
+    yoda: "error",
+    curly: "error",
+    "prefer-template": "error",
+    "max-lines-per-function": ["warn", 50],
+    "@typescript-eslint/explicit-module-boundary-types": "error",
+    "@typescript-eslint/explicit-function-return-type": ["warn", {
+      allowExpressions: true,
+      allowTypedFunctionExpressions: true,
+      allowHigherOrderFunctions: true,
+      allowDirectConstAssertionInArrowFunctions: true,
+      allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+    }],
+    "@typescript-eslint/no-var-requires": "warn",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+    "no-async-promise-executor": "warn",
+
+    "prettier/prettier": "error",
+
+    "no-extra-boolean-cast": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+
+    "require-jsdoc": [
+      "warn",
+      {
+        require: {
+          ClassDeclaration: true,
+          FunctionDeclaration: true,
+          // MethodDefinition: true, // TODO:
+        },
+      },
+    ],
+  },
+};
